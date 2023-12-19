1844ccd403393f9e2ef19d3045d4036b044905ee
esma <esma.ea.aksoy@gmail.com>
2023-12-19 21:26:34 +0300

    import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from 'react'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Projects from './pages/projects/Projects'
+import Contact from './pages/contact/Contact'
+import NotFound from './components/Error/NotFound'
+import {Routes, Route} from "react-router-dom"
+import "./scss/main.scss"
 function App() {
   return (
-    <div >
-   
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/projects" element={<Projects />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
